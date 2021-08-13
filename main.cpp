e49141e2192fb8df494fc8bcef2f632aa9d3b6fd
3m3rs0n <emersonchipana12345@gmail.com>
2021-08-13 14:43:15 -0400

    Agregando Modulo Residuo

--- main.cpp
+++ main.cpp
@@ -35,7 +35,11 @@ int capicua(int N) {
 /* Retornar el residuo de una division sin tulizar el operado residuo (modulo %) ni el operador de division (/)
 */
 int residuo(int a, int b) {
-    return 0;
+		int r=0;
+	while(r<a-b){
+		r=r+b;
+	}
+	return a-r;
 }
 
 
